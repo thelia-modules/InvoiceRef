fb5f87a28d4336895cd1c26a690621b6a27109c8
Gilles <gilles.bourgeat@gmail.com>
2019-01-22 10:43:02 +0100

    Fix module activation

--- InvoiceRef.php
+++ InvoiceRef.php
@@ -12,22 +12,30 @@
 
 namespace InvoiceRef;
 
+use Propel\Runtime\ActiveQuery\Criteria;
 use Propel\Runtime\Connection\ConnectionInterface;
 use Thelia\Model\ConfigQuery;
+use Thelia\Model\OrderQuery;
 use Thelia\Module\BaseModule;
 
 class InvoiceRef extends BaseModule
 {
     const DOMAIN_NAME = "invoiceref";
-    /*
-     * You may now override BaseModuleInterface methods, such as:
-     * install, destroy, preActivation, postActivation, preDeactivation, postDeactivation
-     *
-     * Have fun !
-     */
 
     public function postActivation(ConnectionInterface $con = null)
     {
-        ConfigQuery::write('invoiceRef', 1, true, true);
+        if (null === ConfigQuery::read('invoiceRef', null)) {
+            if (null !== $lastOderPaid = OrderQuery::create()
+                ->filterByInvoiceRef(null, Criteria::NOT_EQUAL)
+                ->orderByInvoiceRef(Criteria::DESC)
+                ->findOne()) {
+                $nextRef = (int) $lastOderPaid->getInvoiceRef();
+                $nextRef++;
+
+                ConfigQuery::write('invoiceRef', $nextRef, true, true);
+            } else {
+                ConfigQuery::write('invoiceRef', 1, true, true);
+            }
+        }
     }
 }
